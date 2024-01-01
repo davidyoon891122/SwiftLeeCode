e5b724a4a3fe60af710fcc2764b12b3c8f60d836
David Yoon <qqpo12@naver.com>
2024-01-01 17:48:51 +0900

    Initial Commit

---
+++ SwiftAlgorithm/SwiftAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAlgorithmApp.swift
+//  SwiftAlgorithm
+//
+//  Created by Jiwon Yoon on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
